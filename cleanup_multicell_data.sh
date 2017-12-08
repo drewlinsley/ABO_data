1bb283a23021be3b6712b42b94c78538f95058bb
Drew Linsley <drewlinsley@gmail.com>
2017-12-08 09:19:08 -0500

    Updated cleanup script.

--- cleanup_multicell_data.sh
+++ cleanup_multicell_data.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-cd /home/drew/Documents/abo_data
+abo_dir=/home/drew/Documents/abo_data
+ccbp_dir=/home/drew/Documents/contextual_circuit_bp
+
+cd $abo_dir
 rm -rf multi_cell_exps/*
-cd /home/drew/Documents/contextual_circuit_bp
+cd $ccbp_dir
 rm -rf dataset_processing/MULTIALLEN*
 rm -rf models/structs/MULTIALLEN*
 cp experiments.py.backup experiments.py
 python prepare_experiments.py --initialize
-cd /home/drew/Documents/abo_data
+cd $abo_dir
 
